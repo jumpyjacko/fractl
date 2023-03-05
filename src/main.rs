a7f3ad0943a6190738bbe393e28b48612752f812
Jackson Ly <48436180+JumpyJacko@users.noreply.github.com>
2023-03-05 11:44:43 +1100

    Fmt + while -> for loop

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,59 @@
-use image::{ImageBuffer, RgbImage, Rgb};
 use colorgrad::{Color, Gradient};
+use image::{ImageBuffer, Rgb, RgbImage};
 
 #[derive(Debug, Clone, Copy)]
-struct Vec2 {x: f64, y: f64}
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
 
 #[derive(Debug, Clone, Copy)]
-struct IVec2 {x: u64, y: u64}
+struct IVec2 {
+    x: u64,
+    y: u64,
+}
+
+enum Fractal {
+    Julia,
+    Mandelbrot,
+}
 
 fn main() {
-    // let constant = Vec2 { x: -0.4, y: 0.6};
-    let constant = Vec2 { x: -0.8, y: 0.156 };
     let image_size = IVec2 { x: 1920, y: 1080 };
     let grayscale = colorgrad::CustomGradient::new()
         .colors(&[
             Color::from_rgba8(0, 0, 0, 255),
             Color::from_rgba8(255, 255, 255, 255),
         ])
-        .build().unwrap();
+        .build()
+        .unwrap();
+    let output_buffer: RgbImage = ImageBuffer::new(
+        image_size.x.try_into().unwrap(),
+        image_size.y.try_into().unwrap(),
+    );
 
+    // let constant = Vec2 { x: -0.4, y: 0.6};
+    let constant = Vec2 { x: -0.8, y: 0.156 };
 
-    let output_buffer: RgbImage = ImageBuffer::new(image_size.x.try_into().unwrap(), image_size.y.try_into().unwrap());
-    
-    render(image_size, constant, 200, output_buffer, grayscale);
-    // render(image_size, constant, 200, output_buffer, colorgrad::magma());
+    render(
+        image_size,
+        constant,
+        200,
+        output_buffer,
+        grayscale,
+        Fractal::Julia,
+        "julia.png",
+    );
 }
 
 fn compute_next_julia(current: Vec2, constant: Vec2) -> Vec2 {
     let z_real = (current.x * current.x) - (current.y * current.y);
     let z_imaginary = 2.0 * current.x * current.y;
 
-    Vec2 {x: z_real + constant.x, y: z_imaginary + constant.y}
+    Vec2 {
+        x: z_real + constant.x,
+        y: z_imaginary + constant.y,
+    }
 }
 
 fn modulus_squared(z: Vec2) -> f64 {
@@ -48,26 +72,43 @@ fn iterate_to_max_julia(initial_z: Vec2, constant: Vec2, max_iterations: usize) 
     iteration
 }
 
-fn render(render_size: IVec2, constant: Vec2, max_iterations: usize, mut image: RgbImage, gradient: Gradient) {
+fn render(
+    render_size: IVec2,
+    constant: Vec2,
+    max_iterations: usize,
+    mut image: RgbImage,
+    gradient: Gradient,
+    fractal_type: Fractal,
+    out_name: &str,
+) {
     let scale = 1.0 / (render_size.y as f64 / 2.0);
-    let mut y = 0;
-    while y < render_size.y {
-        let mut x = 0;
-        while x < render_size.x {
+    for y in  0..render_size.y {
+        for x in 0..render_size.x {
             let pixel_x = (x as f64 - render_size.x as f64 / 2.0) * scale;
             let pixel_y = (y as f64 - render_size.y as f64 / 2.0) * scale;
 
-            let iterations = iterate_to_max_julia(Vec2 { x: pixel_x, y: pixel_y }, constant, max_iterations);
+            let iterations = match fractal_type {
+                Fractal::Julia => iterate_to_max_julia(
+                    Vec2 {
+                        x: pixel_x,
+                        y: pixel_y,
+                    },
+                    constant,
+                    max_iterations,
+                ),
+                _ => todo!(),
+            };
 
             let normalised_value = iterations as f64 / max_iterations as f64;
             let colour = gradient.at(normalised_value).to_rgba8();
 
-            image.put_pixel(x.try_into().unwrap(), y.try_into().unwrap(), Rgb([colour[0], colour[1], colour[2]]));
-
-            x += 1;
+            image.put_pixel(
+                x.try_into().unwrap(),
+                y.try_into().unwrap(),
+                Rgb([colour[0], colour[1], colour[2]]),
+            );
         }
-        y += 1;
     }
 
-    image.save("output.png").unwrap();
+    image.save(out_name).unwrap();
 }
